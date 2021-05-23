31931b3b1da4842ecd272d13f025547799b16959
CptnCrumble <paul_hazell@live.co.uk>
2021-05-23 21:18:41 +0100

    grind it out

--- validation.js
+++ validation.js
@@ -24,28 +24,120 @@ const hadsValidation = {
     ]
 }
 
-// const Validation = {
-//     'allowedValues':[],
-//     'keys': []
-// }
+const nmsValidation = {
+    'allowedValues':[0,1],
+    'keys': [
+        'NMS1',
+        'NMS2',
+        'NMS3',
+        'NMS4',
+        'NMS5',
+        'NMS6',
+        'NMS7',
+        'NMS8',
+        'NMS9',
+        'NMS10',
+        'NMS11',
+        'NMS12',
+        'NMS13',
+        'NMS14',
+        'NMS15',
+        'NMS16',
+        'NMS17',
+        'NMS18',
+        'NMS19',
+        'NMS20',
+        'NMS21',
+        'NMS22',
+        'NMS23',
+        'NMS24',
+        'NMS25',
+        'NMS26',
+        'NMS27',
+        'NMS28',
+        'NMS29',
+        'NMS30',
+        'NMS31'
+    ]
+}
 
-// const Validation = {
-//     'allowedValues':[],
-//     'keys': []
-// }
+const pdqcValidation = {
+    'allowedValues':[0,1,2,3,4],
+    'keys': [
+        'pdqc 1',
+        'pdqc 2',
+        'pdqc 3',
+        'pdqc 4',
+        'pdqc 5',
+        'pdqc 6',
+        'pdqc 7',
+        'pdqc 8',
+        'pdqc 9',
+        'pdqc 10',
+        'pdqc 11',
+        'pdqc 12',
+        'pdqc 13',
+        'pdqc 14',
+        'pdqc 15',
+        'pdqc 16',
+        'pdqc 17',
+        'pdqc 18',
+        'pdqc 19',
+        'pdqc 20',
+        'pdqc 21',
+        'pdqc 22',
+        'pdqc 23',
+        'pdqc 24',
+        'pdqc 25',
+        'pdqc 26',
+        'pdqc 27',
+        'pdqc 28',
+        'pdqc 29'
+    ]
+}
 
-// const Validation = {
-//     'allowedValues':[],
-//     'keys': []
-// }
+const updrsValidation = {
+    'allowedValues':[0,1,2,3,4],
+    'keys': [
+        '2.1',
+        '2.2',
+        '2.3',
+        '2.4',
+        '2.5',
+        '2.6',
+        '2.7',
+        '2.8',
+        '2.9',
+        '2.10',
+        '2.11',
+        '2.12',
+        '2.13'
+    ]
+}
 
-// const Validation = {
-//     'allowedValues':[],
-//     'keys': []
-// }
+const pdssValidation = {
+    'allowedValues':[0,1,2,3,4],
+    'keys': [
+        'PDSS1',
+        'PDSS2',
+        'PDSS3',
+        'PDSS4',
+        'PDSS5',
+        'PDSS6',
+        'PDSS7',
+        'PDSS8',
+        'PDSS9',
+        'PDSS10',
+        'PDSS11',
+        'PDSS12',
+        'PDSS13',
+        'PDSS14',
+        'PDSS15'
+    ]
+}
 
-// const Validation = {
-//     'allowedValues':[],
+// const pdq8Validation = {
+//     'allowedValues':[0,1,2,3],
 //     'keys': []
 // }
 
@@ -77,5 +169,5 @@ function validate(validationObject,dataObject) {
 }
 
 data.map(x => {
-    if(!validate(hadsValidation,x)) {console.log(x['Number'],x['Timepoint'])}
+    if(!validate(pdssValidation,x)) {console.log(x['Number'],x['Timepoint'])}
 });
